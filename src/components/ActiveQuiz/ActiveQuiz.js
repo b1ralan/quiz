5fb1c188d4605e9a48470fcb8a4ebbfc952a8046
Alexandr Biryukov <biralan.mj@gmail.com>
2021-06-09 18:20:05 +0500

    Quiz

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './ActivQuiz.module.css';
+import AnswerList from "./AnswerList/AnswerList";
+
+const ActiveQuiz = props => {
+    return(
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>{props.answerNumber}</strong>&nbsp;
+                {props.question}
+            </span>
+
+            <small>{props.answerNumber} из {props.quizLength}</small>
+        </p>
+
+        <AnswerList
+            answers={props.answers}
+            onAnswerClick={props.onAnswerClick}
+            state={props.state}
+        />
+    </div>
+)}
+
+export default ActiveQuiz;
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import classes from './Auth.module.css'
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Imput/Input";
+import {connect} from "react-redux";
+import {auth} from "../../store/actions/auth";
+
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+class Auth extends React.Component {
+
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
+
+    loginHandler =  () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            true
+        )
+
+    };
+    registerHandler =  () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false
+        )
+
+
+    };
+    submitHandler = event => {
+        event.preventDefault()
+    };
+
+
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true;
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.email) {
+            isValid = validateEmail(value) && isValid
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
+
+    onChangeHandler = (event, nameControl) => {
+
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[nameControl]}
+
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[nameControl] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name=>{
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls, isFormValid
+        })
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((nameControl, index) => {
+            const control = this.state.formControls[nameControl]
+            return (
+                <Input
+                    key={nameControl + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!control.validation}
+                    errorMessage={control.errorMessage}
+                    onChange={event => this.onChangeHandler(event, nameControl)}
+                />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+
+                        {this.renderInputs()}
+
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                            disable={!this.state.isFormValid}
+                        >
+                            Войти
+                        </Button>
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                            disable={!this.state.isFormValid}
+                        >
+                            Регистрация
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        auth: (email,password, isLogin) => dispatch(auth(email,password, isLogin))
+    }
+}
+
+export default connect(null,mapDispatchToProps)(Auth);
